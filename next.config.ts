f16e6e05b2fa6d3a530d7fffceb491229c43a406
thisisshivamtiwari <shivamtiwari.1527@gmail.com>
2025-04-24 18:19:47 +0530

    Update Next.js config for Ant Design compatibility

--- next.config.ts
+++ next.config.ts
@@ -16,6 +16,18 @@ const nextConfig: NextConfig = {
         protocol: 'https',
         hostname: 'assets.aceternity.com',
       },
+      {
+        protocol: 'https',
+        hostname: 'rown-space-bucket.nyc3.digitaloceanspaces.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'example.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'static.wixstatic.com',
+      }
     ],
   },
 };
